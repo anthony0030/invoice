8d78612db01164dcc2c66c2d9956f6f003be502b
Anthony Veaudry <anthonyveaudry@gmail.com>
2015-10-23 20:21:44 +0200

    java script translator

---
+++ assets/js/lang.js
@@ -0,0 +1,27 @@
+function changelanggr()
+{
+    document.getElementById("date").className = "dategr";
+    document.getElementById("amount").className = "amountgr";
+    document.getElementById("item").className = "itemgr";
+    document.getElementById("description").className = "descriptiongr";
+    document.getElementById("rate").className = "rategr";
+    document.getElementById("quantity").className = "quantitygr";
+    document.getElementById("price").className = "pricegr";
+    document.getElementById("subtotal").className = "subtotalgr";
+    document.getElementById("tax").className = "taxgr";
+    document.getElementById("total").className = "totalgr";
+}
+function changelangen()
+{
+    document.getElementById("date").className = "date";
+    document.getElementById("amount").className = "amount";
+    document.getElementById("item").className = "item";
+    document.getElementById("description").className = "description";
+    document.getElementById("rate").className = "rate";
+    document.getElementById("quantity").className = "quantity";
+    document.getElementById("price").className = "price";
+    document.getElementById("subtotal").className = "subtotal";
+    document.getElementById("tax").className = "tax";
+    document.getElementById("total").className = "total";
+}
+
