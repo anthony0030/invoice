dabdf9568a4689939bde9ee7a089d678418d67f9
Anthony Veaudry <anthonyveaudry@gmail.com>
2015-10-24 11:31:16 +0200

    add backup of assets

---
+++ assets/js/cpanel/num.js
@@ -0,0 +1,17 @@
+function changenumon()
+{
+
+    document.getElementById("numon").style.display = "none";
+    document.getElementById("num2").style.display = "table-row";
+    document.getElementById("numon").style.display = "none";
+    document.getElementById("numoff").style.display = "block";
+}
+
+function changenumoff()
+{
+    document.getElementById("numoff").style.display = "block";
+    document.getElementById("num2").style.display = "none";
+    document.getElementById("numoff").style.display = "none";
+    document.getElementById("numon").style.display = "block";
+}
+
