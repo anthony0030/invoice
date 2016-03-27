32f611446d2f49b74f86ba273b3f00c86174f642
Anthony Veaudry <anthonyveaudry@gmail.com>
2016-03-27 14:05:59 +0300

    change id from "num3" to "total_block"

--- assets/js/cpanel/euro.js
+++ assets/js/cpanel/euro.js
@@ -2,7 +2,7 @@ function changeeuroon()
 {
 
 	document.getElementById("balance").style.display = "table";
-    document.getElementById("num3").style.display = "table-row";
+    document.getElementById("total_block").style.display = "table-row";
     document.getElementById("euroon").className = "active";
     document.getElementById("eurooff").className = "";
 }
@@ -10,7 +10,7 @@ function changeeuroon()
 function changeeurooff()
 {
 	document.getElementById("balance").style.display = "none";
-    document.getElementById("num3").style.display = "none";
+    document.getElementById("total_block").style.display = "none";
     document.getElementById("eurooff").className = "active";
     document.getElementById("euroon").className = "";
 }
