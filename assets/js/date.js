fde4d6cc7df36f2ba660d39df29a182992644ccd
Anthony Veaudry <anthonyveaudry@gmail.com>
2017-06-22 09:13:35 +0300

    add missing semiclolons

--- assets/js/date.js
+++ assets/js/date.js
@@ -4,11 +4,11 @@ var mm = today.getMonth()+1; //January is 0!
 var yyyy = today.getFullYear();
 
 if(dd<10) {
-    dd='0'+dd
+    dd='0'+dd;
 } 
 
 if(mm<10) {
-    mm='0'+mm
+    mm='0'+mm;
 } 
 
 today = dd+'/'+mm+'/'+yyyy;
