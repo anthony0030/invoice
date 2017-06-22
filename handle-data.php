e21451c9c2afc7da671fbf5dcfb13e26e159f3ac
Anthony Veaudry <anthonyveaudry@gmail.com>
2017-06-22 09:04:42 +0300

    remove php

--- handle-data.php
+++
@@ -1,4 +0,0 @@
-Date: <?php echo $_POST["date"]; ?> <br>
-Serial Number: <?php echo $_POST["serial"]; ?> <br>
-Company Name: <?php echo $_POST["company_name"]; ?> <br>
-Customer Name<?php echo $_POST["customer_name"]; ?> <br>
